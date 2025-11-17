551046c41ef7a33cb6cb9206666cfddd31f69050
Your Name <you@example.com>
2025-11-17 14:47:34 +0200

    Initial provider release

---
+++ provider.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+    "context"
+
+    "github.com/hashicorp/terraform-plugin-framework/provider"
+    "github.com/hashicorp/terraform-plugin-framework/provider/schema"
+    "github.com/hashicorp/terraform-plugin-framework/resource"
+    "github.com/hashicorp/terraform-plugin-framework/datasource"
+)
+
+type EnvSendProvider struct{}
+
+func NewProvider() provider.Provider {
+    return &EnvSendProvider{}
+}
+
+func (p *EnvSendProvider) Metadata(_ context.Context, _ provider.MetadataRequest, resp *provider.MetadataResponse) {
+    resp.TypeName = "envsend"
+}
+
+func (p *EnvSendProvider) Schema(_ context.Context, _ provider.SchemaRequest, resp *provider.SchemaResponse) {
+    resp.Schema = schema.Schema{}
+}
+
+func (p *EnvSendProvider) Configure(_ context.Context, _ provider.ConfigureRequest, _ *provider.ConfigureResponse) {
+    // No-op
+}
+
+func (p *EnvSendProvider) Resources(_ context.Context) []func() resource.Resource {
+    return []func() resource.Resource{
+        NewEnvSendResource,
+    }
+}
+
+func (p *EnvSendProvider) DataSources(_ context.Context) []func() datasource.DataSource {
+    return nil
+}
